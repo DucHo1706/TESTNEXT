b33bcc98607640a27197dde357443718b9eccafd
duc18 <duc18@DUCKR5>
2024-11-09 07:54:51 +0700

    Add project files.

---
+++ TESTNEXT/Controllers/Bai_vietController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TESTNEXT.Models;
+
+namespace TESTNEXT.Controllers
+{
+
+    public class Bai_vietController : Controller
+    {
+        private TESTEntities db = new TESTEntities();
+
+        // GET: Bai_viet
+
+        public ActionResult Index()
+        {
+
+            var bai_viet = db.Bai_viet.Include(b => b.Danh_Muc).Include(b => b.User);
+            return View(bai_viet.ToList());
+        }
+
+        // GET: Bai_viet/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bai_viet bai_viet = db.Bai_viet.Find(id);
+            if (bai_viet == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bai_viet);
+        }
+
+        // GET: Bai_viet/Create
+        public ActionResult Create()
+        {
+            ViewBag.DanhMucID = new SelectList(db.Danh_Muc, "DanhMucID", "TenDanhMuc");
+            ViewBag.UserID = new SelectList(db.Users, "UserID", "UserName");
+            return View();
+        }
+
+        // POST: Bai_viet/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "BaiVietID,TieuDe,NoiDung,DanhMucID,Hinh_Anh,Thoi_Gian_Tao,UserID")] Bai_viet bai_viet)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Bai_viet.Add(bai_viet);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.DanhMucID = new SelectList(db.Danh_Muc, "DanhMucID", "TenDanhMuc", bai_viet.DanhMucID);
+            ViewBag.UserID = new SelectList(db.Users, "UserID", "UserName", bai_viet.UserID);
+            return View(bai_viet);
+        }
+
+        // GET: Bai_viet/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bai_viet bai_viet = db.Bai_viet.Find(id);
+            if (bai_viet == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.DanhMucID = new SelectList(db.Danh_Muc, "DanhMucID", "TenDanhMuc", bai_viet.DanhMucID);
+            ViewBag.UserID = new SelectList(db.Users, "UserID", "UserName", bai_viet.UserID);
+            return View(bai_viet);
+        }
+
+        // POST: Bai_viet/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "BaiVietID,TieuDe,NoiDung,DanhMucID,Hinh_Anh,Thoi_Gian_Tao,UserID")] Bai_viet bai_viet)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(bai_viet).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.DanhMucID = new SelectList(db.Danh_Muc, "DanhMucID", "TenDanhMuc", bai_viet.DanhMucID);
+            ViewBag.UserID = new SelectList(db.Users, "UserID", "UserName", bai_viet.UserID);
+            return View(bai_viet);
+        }
+
+        // GET: Bai_viet/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bai_viet bai_viet = db.Bai_viet.Find(id);
+            if (bai_viet == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bai_viet);
+        }
+
+        // POST: Bai_viet/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Bai_viet bai_viet = db.Bai_viet.Find(id);
+            db.Bai_viet.Remove(bai_viet);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }   
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ TESTNEXT/Controllers/HomeController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using TESTNEXT.Models;
+
+namespace TESTNEXT.Controllers
+{
+   
+    public class HomeController : Controller
+    {
+        private TESTEntities db = new TESTEntities();
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            if (Session["Role"] != null)
+            {
+                if (Session["Role"].ToString() == "Admin")
+                {
+                    // Nếu là Admin, trả về view About
+                    ViewBag.Message = "Your application description page for Admin.";
+                    return View();
+                }
+                else if (Session["Role"].ToString() == "User ")
+                {
+                    // Nếu là User, có thể chuyển hướng đến view Index hoặc một view khác
+                    return RedirectToAction("Index");
+                }
+            }
+
+            // Nếu không có quyền, trả về một view thông báo
+            ViewBag.Message = "Bạn không được phép truy cập";
+            return View("Login"); // Giả sử bạn có một view AccessDenied
+        }
+
+        public ActionResult Contact()
+        {
+            if (Session["Role"] != null)
+            {
+                if (Session["Role"].ToString() == "Admin")
+                {
+                    // Nếu là Admin, trả về view About
+                    ViewBag.Message = "Your application description page for Admin.";
+                    return View();
+                }
+                else if (Session["Role"].ToString() == "User ")
+                {
+                    // Nếu là User, có thể chuyển hướng đến view Index hoặc một view khác
+                    return RedirectToAction("Index");
+                }
+            }
+
+            // Nếu không có quyền, trả về một view thông báo
+            ViewBag.Message = "Bạn không được phép truy cập";
+            return View("Index"); // Giả sử bạn có một view AccessDenied
+        }
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Login(User user)
+        {
+            using (var db = new TESTEntities())
+            {
+                var validUser = db.Users.FirstOrDefault(u => u.UserName == user.UserName && u.UserPassword == user.UserPassword);
+                if (validUser != null)
+                {
+                    FormsAuthentication.SetAuthCookie(validUser.UserName, false);
+                    Session["Role"] = validUser.RoleUser;  // Lưu vai trò người dùng trong session
+                    Session["User Name"] =validUser.UserName;
+                    return RedirectToAction("Index", "Home");
+                }
+                ViewBag.ErrorMessage = "Invalid Username or Password";
+                return View();
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            Session["Role"] = null;  // Xóa session vai trò người dùng
+            return RedirectToAction("Login", "Home");
+        }
+
+    }
+}
